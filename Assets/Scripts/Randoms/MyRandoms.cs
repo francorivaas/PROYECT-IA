c34d9882ff4467ea849cf1999f9e0fad42075314
francorivaas <65259655+francorivaas@users.noreply.github.com>
2023-05-08 19:02:41 -0300

    roulette working

--- Assets/Scripts/Randoms/MyRandoms.cs
+++ Assets/Scripts/Randoms/MyRandoms.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class MyRandoms : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static float Range(float min, float max)
     {
-        
+        return Random.value * (max - min) + min;
     }
 
-    // Update is called once per frame
-    void Update()
+    public static T Roulette<T>(Dictionary<T, float> items)
     {
+        float total = 0;
+        foreach (var item in items)
+        {
+            //al final del foreach el total va a ser efectivamente
+            //la suma de todos los items.
+            total += item.Value;
+        }
+        var random = Range(0, total);
+
+        foreach (var item in items)
+        {
+            if (random < item.Value)
+            {
+                return item.Key;
+            }
+            else
+            {
+                random -= item.Value;
+            }
+        
+        }
+        return default(T);
+
         
     }
 }
---
+++ Assets/Scripts/Randoms/Test.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    public Dictionary<string, float> dic = new Dictionary<string, float>();
+    private void Start()
+    {
+        dic["Crash"] = 100; 
+        dic["Drake"] = 45; 
+        dic["Sonic"] = 20; 
+        dic["Plomero"] = 1; 
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            //var random = MyRandoms.Range(50, 100);
+            //print(random.ToString());
+
+            for (int i = 0; i < 100; i++)
+            {
+                var item = MyRandoms.Roulette(dic);
+                print(item);
+            }
+        }
+    }
+}
