51ba16595880167d1b647421bf9f81cae3c09624
francorivaas <65259655+francorivaas@users.noreply.github.com>
2023-05-25 16:38:01 -0300

    some messages on the canvas

--- Assets/Scripts/Controllers/VictoryTrigger.cs
+++ Assets/Scripts/Controllers/VictoryTrigger.cs
@@ -4,6 +4,29 @@ using UnityEngine;
 
 public class VictoryTrigger : MonoBehaviour
 {
+    [SerializeField] private GameObject noKeyText;
+    private float timeToShowText = 5.0f;
+    private float currentTimeToShowText;
+    private bool canCount;
+
+    private void Start()
+    {
+        currentTimeToShowText = timeToShowText;    
+    }
+
+    private void Update()
+    {
+        if (canCount)
+        {
+            currentTimeToShowText -= Time.deltaTime;
+            if (currentTimeToShowText <= 0)
+            {
+                noKeyText.SetActive(false);
+                currentTimeToShowText = timeToShowText;
+                canCount= false;
+            }
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.GetComponent<PlayerModel>() != null
@@ -11,10 +34,12 @@ public class VictoryTrigger : MonoBehaviour
         {
             GameManager.instance.LoadGameScene();
         }
-        else if (other.gameObject.GetComponent<PlayerModel>() != null 
+        else if (other.gameObject.GetComponent<PlayerModel>() != null
             && !GameManager.hasKey)
         {
-            print("no key");
+            canCount = true;
+            noKeyText.SetActive(true);
         }
+        else noKeyText.SetActive(false);
     }
 }
