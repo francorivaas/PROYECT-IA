ba2ff2679adab24c45020cd1ee92964ad377ef3a
francorivaas <65259655+francorivaas@users.noreply.github.com>
2023-05-25 16:06:56 -0300

    as

--- Assets/Scripts/Weapon/Gun.cs
+++ Assets/Scripts/Weapon/Gun.cs
@@ -7,18 +7,5 @@ public class Gun : Weapon
     public override void Shoot(Transform firepoint)
     {
         base.Shoot(firepoint);
-        RaycastHit hit;
-        if (Physics.Raycast(firepoint.transform.position, firepoint.transform.forward, out hit, range))
-        {
-            print(hit.transform.name + hit.transform.position);
-
-            LifeController enemy = hit.transform.GetComponent<LifeController>();
-            if (enemy != null)
-            {
-                print("enemy found!");
-                enemy.TakeDamage(damage);
-            }
-            else print("no enemy");
-        }
     }
 }
--- Assets/Scripts/Weapon/Weapon.cs
+++ Assets/Scripts/Weapon/Weapon.cs
@@ -7,12 +7,20 @@ public class Weapon : MonoBehaviour
     public int damage;
     public float range;
 
-    private void Start()
-    {
-    }
-
     public virtual void Shoot(Transform firePoint)
     {
+        RaycastHit hit;
+        if (Physics.Raycast(firePoint.transform.position, firePoint.transform.forward, out hit, range))
+        {
+            print(hit.transform.name + hit.transform.position);
 
+            LifeController enemy = hit.transform.GetComponent<LifeController>();
+            if (enemy != null)
+            {
+                print("enemy found!");
+                enemy.TakeDamage(damage);
+            }
+            else print("no enemy");
+        }
     }
 }
